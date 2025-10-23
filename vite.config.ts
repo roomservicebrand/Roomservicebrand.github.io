5cd876f29d17a52494789741714ef8fa711093b6
Amit Mahapatra <217933726+roomservicebrand@users.noreply.github.com>
2025-10-22 23:37:10 -0500

    fix: Update canonical URL and Vite config
    Updates the canonical URL in `index.html` to the live domain.
    Adjusts `vite.config.ts` to set the `root` to `src` and correctly configure the `outDir` for the build process.

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: 'src',
   build: {
-    outDir: 'dist',        // no need for ../ because root is same folder
+    outDir: '../dist',
     emptyOutDir: true,
   },
 });
