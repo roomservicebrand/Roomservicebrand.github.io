b39a77b77ff41563112631aea3acd4cc02e737f4
Amit Mahapatra <217933726+roomservicebrand@users.noreply.github.com>
2025-10-23 09:08:31 -0500

    Set relative asset base for Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   // The build output is configured to go to the project's root `dist` directory.
   build: {
